ac50412a8f4534bda5ec74a8043c21c518ae3f26
felipezora <juanfzz2001@gmail.com>
2022-02-04 15:24:15 -0500

    corrección errores eslint

--- src/components/PresentationCard.jsx
+++ src/components/PresentationCard.jsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const PresentationCard = ({ profName, profRole, profImg }) => {
   return (
-    <>
-      <div className="w-full flex items-center justify-center">
-        <div className="xl:w-1/4 sm:w-1/2 w-full 2xl:w-1/5 flex flex-col items-center py-16 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500 rounded-lg">
-          <div className="w-full flex items-center justify-center">
-            <div className="flex flex-col items-center">
-              <img src={profImg} alt="profile" />
-            </div>
+    <div className='w-full flex items-center justify-center'>
+      <div className='xl:w-1/4 sm:w-1/2 w-full 2xl:w-1/5 flex flex-col items-center py-16 md:py-12 bg-gradient-to-r from-indigo-700 to-purple-500 rounded-lg'>
+        <div className='w-full flex items-center justify-center'>
+          <div className='flex flex-col items-center'>
+            <img src={profImg} alt='profile' />
           </div>
-          <div className="grid mt-7 justify-items-center">
-            <div className="py-1">
-              <p className="text-xl text-gray-100 text-center">{profName}</p>
-            </div>
-            <div className="py-1">
-              <p className="text-lg text-gray-100 text-center">{profRole}</p>
-            </div>
+        </div>
+        <div className='grid mt-7 justify-items-center'>
+          <div className='py-1'>
+            <p className='text-xl text-gray-100 text-center'>{profName}</p>
+          </div>
+          <div className='py-1'>
+            <p className='text-lg text-gray-100 text-center'>{profRole}</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "../styles/styles.css";
+import React from 'react';
+import 'styles/styles.css';
 
 const TextCard = ({ title, text }) => {
   return (
-    <div className="bg-gray-200 rounded-lg border-2 border-black justify-self-center w-3/4 my-1">
-      <span className="fuenteTitulo">{title}</span>
-      <p className="fuenteBase">{text}</p>
+    <div className='bg-gray-200 rounded-lg border-2 border-black justify-self-center w-3/4 my-1'>
+      <span className='fuenteTitulo'>{title}</span>
+      <p className='fuenteBase'>{text}</p>
     </div>
   );
 };
